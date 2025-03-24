0f9e6324064a49983469ef116e476d081d3c84d3
DevCrypto187 <crypto_dev@marzmain.io>
2025-03-24 20:40:16 +0900

    jetfuel modify tonconnect

--- utils/tonConnect.js
+++ utils/tonConnect.js
@@ -7,7 +7,7 @@ let tonConnect = null;
 export const getTonConnectInstance = () => {
   if (!tonConnect) {
     tonConnect = new TonConnect({
-      manifestUrl: "https://mgdggame.vercel.app/tonconnect-manifest.json",
+      manifestUrl: "https://jetfuelgame.vercel.app/tonconnect-manifest.json",
     });
   }
   return tonConnect;
